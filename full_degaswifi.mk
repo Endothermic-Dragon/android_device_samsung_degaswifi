aeb3105ec71a92eed301d6011b42eace6fb04cc1
Eshaan Debnath <69125881+Endothermic-Dragon@users.noreply.github.com>
2025-05-13 02:13:38 -0400

    Update full_degaswifi.mk

--- full_degaswifi.mk
+++ full_degaswifi.mk
@@ -19,7 +19,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
 $(call inherit-product, device/samsung/degaswifi/device.mk)
 
 # Set those variables here to overwrite the inherited values.
-PRODUCT_NAME := full_degaswifi
+PRODUCT_NAME := lineage_degaswifi
 PRODUCT_DEVICE := degaswifi
 PRODUCT_BRAND := samsung
 PRODUCT_MANUFACTURER := samsung
